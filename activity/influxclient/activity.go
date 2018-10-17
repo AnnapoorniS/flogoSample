5259dc11d0b92015377062c00291fe619682a087
Annapoorni.S <Annapoorni.S@target.com>
2018-10-17 09:35:08 +0530

    Test

--- activity/influxclient/activity.go
+++ activity/influxclient/activity.go
@@ -30,7 +30,7 @@ func (a *MyActivity) Metadata() *activity.Metadata {
 func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 
 	// do eval
-	//messages := context.GetInput("data").(map[string]interface{})
+	fields := context.GetInput("data").(map[string]interface{})
 	database := context.GetInput("database").(string)
 	server_address := context.GetInput("addr").(string)
 
@@ -52,13 +52,8 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 	})
 
 	// Create a point and add to batch
-	tags := map[string]string{"cpu": "cpu-total"}
-	fields := map[string]interface{}{
-		"idle":   98.1,
-		"system": 98.3,
-		"user":   98.6,
-	}
-	pt, err := client.NewPoint("cpu_usage", tags, fields, time.Now())
+	tags := map[string]string{"test": "test-message"}
+	pt, err := client.NewPoint("test_msg", tags, fields, time.Now())
 	if err != nil {
 		fmt.Println("Error: ", err.Error())
 	}
